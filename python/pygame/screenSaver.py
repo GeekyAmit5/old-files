39649a3747254a53177d046a7d7f8e646e7a371a
GeekyAmit5 <amitmishra95820@gmail.com>
2022-01-22 22:44:06 +0530

    screenSaver OOP

--- python/pygame/screenSaver.py
+++ python/pygame/screenSaver.py
@@ -5,45 +5,45 @@ import math
 import time
 import pygame
 
-pygame.init()
 
-win = pygame.display.set_mode((500, 500))
+class rect:
+    def __init__(self, w, h):
+        self.w = w
+        self.h = h
+
+    def display(self, x, y):
+        pygame.draw.rect(win, (255, 0, 0), (x, y, w, h))
+
+
+pygame.init()
+width = 600
+height = 400
+win = pygame.display.set_mode((width, height))
 pygame.display.set_caption("My Game")
 
-x = rd.randint(0, 450)
-y = rd.randint(0, 450)
-h = 50
-w = 30
-v = 0.3
+h = 80
+w = 50
+xv = 0.25
+yv = 0.15
+x = rd.randint(0, width-w)
+y = rd.randint(0, height-h)
 
-t = True
-s = True
+rect1 = rect(w, h)
 
 run = True
 while run:
-    pygame.time.delay(1)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-    if x < 470 and t:
-        x += v
-    else:
-        t = False
-        x -= v
-        if x < 0:
-            t = True
-
-    if y < 450 and s:
-        y += v
-    else:
-        s = False
-        y -= v
-        if y < 0:
-            s = True
-
     win.fill((0, 0, 0))
-
-    pygame.draw.rect(win, (255, 0, 0), (x, y, w, h))
+    rect1.display(x, y)
+    x += xv
+    y += yv
+    if x > width - w or x < 0:
+        xv = -xv
+    if y > height - h or y < 0:
+        yv = -yv
+    # pygame.time.delay(1)
     pygame.display.update()
 
 pygame.quit()
